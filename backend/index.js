999489d0cfbc63b4b9d65aa94bb407557b1fb015
santhosh-reddy-126 <126654665+santhosh-reddy-126@users.noreply.github.com>
2025-03-27 09:44:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://movieflow-01.vercel.app/");
+  res.setHeader("Access-Control-Allow-Origin", "https://movieflow-01.vercel.app");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
